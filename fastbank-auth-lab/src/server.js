c5a27bef96c3584cb6a2d434f338afa18011b0da
lmuwakk <laithmuwakki@gmail.com>
2025-12-12 19:06:23 -0500

    server.js

--- fastbank-auth-lab/src/server.js
+++ fastbank-auth-lab/src/server.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
   res.setHeader("Cross-Origin-Resource-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "unsafe-none");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
 
   res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, private");
   res.setHeader("Pragma", "no-cache");
